dff3b79eec4fed9e8b6265eda973171fb90ade40
Syed Furqan Ahmed <sonicfurqan@gmail.com>
2021-01-31 22:31:04 +0530

    Updadating Community Logo Url

--- force-app/main/default/aura/CommunityCustomFooter/CommunityCustomFooterController.js
+++ force-app/main/default/aura/CommunityCustomFooter/CommunityCustomFooterController.js
@@ -1,5 +1,5 @@
 ({
-	onInit : function(component, event, helper) {
-		component.set('v.footerlogo','https://weisshausinvestment--c.documentforce.com/servlet/servlet.ImageServer?id=0155I000000nLMT&oid=00D5I000002GOw0');
+	onInit: function (component, event, helper) {
+		component.set('v.footerlogo', 'https://ev-smartmoney--c.documentforce.com/servlet/servlet.ImageServer?id=0155I000000nLMT&oid=00D5I000002GOw0');
 	}
 })
